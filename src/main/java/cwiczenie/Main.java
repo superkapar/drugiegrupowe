5a8d99c138d5e034f3368bea32ca5b84631da4dd
superkapar <kapceru@gmail.com>
2019-03-02 15:20:44 +0100

    main juz

--- src/main/java/cwiczenie/Main.java
+++ src/main/java/cwiczenie/Main.java
@@ -1,7 +1,32 @@
 package cwiczenie;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wartosc 1:");
+        double liczba1 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Podaj wartosc 2:");
+        double liczba2 = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("wybierz co chcesz zrobic: add/sub/div/mul ");
+        String zdanie = scanner.nextLine();
+        switch (zdanie) {
+            case "add":
+                return;
+            case "sub":
+                return;
+            case "div":
+                return;
+            case "mul":
+                return;
+        }
+
+
+
+
 
     }
 
